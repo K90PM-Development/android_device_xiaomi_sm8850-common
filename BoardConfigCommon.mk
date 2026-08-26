adc71571fdf22309add560ae74a19f20c61d15fe
NullCode1337 <contact.nullcode@proton.me>
2026-08-26 14:58:31 +0600

    sm8850-common: Update qcom sepolicy path
    Change-Id: Ieffb1bbf02a10c0b63f6d0902a6954e470f7b844

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,7 +135,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr/sm8850/SEPolicy.mk
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
